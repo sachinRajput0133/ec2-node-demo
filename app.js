22985f6037dd1e7d7e6b12b7c7af3862e8312262
sachinchib <sachinrajput0133@gmail.com>
2024-05-11 15:06:26 +0700

    git

---
+++ app.js
@@ -0,0 +1,44 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors")
+require('dotenv').config();
+
+// Initialize Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Failed to connect to MongoDB:', err));
+app.use(cors())
+app.use(express.json())
+
+// Define a simple schema and model
+const Schema = mongoose.Schema;
+const ItemSchema = new Schema({
+  name: String,
+});
+
+const ItemModel = mongoose.model('Item', ItemSchema);
+
+// Define a sample API endpoint
+app.get('/api/items', async (req, res) => {
+  try {
+    // Fetch items from MongoDB
+    const items = await ItemModel.find();
+    res.json(items);
+  } catch (err) {
+    console.error('Error fetching items:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
